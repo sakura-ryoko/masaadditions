f658c537fd53a994062d10fb98b7ac2c3d9e29c5
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2025-01-27 22:17:14 -0500

    fix: i18n for Tweakeroo Addon Configs, fix build script; bump versions (#1)
    * index on masaadditions-1.21: d7f5fe1 Merge pull request #32 from CrazyHPi/masaadditions-1.21
    
    * Merge from upstream
    
    * Merge Stash
    
    * Bump i18n fixes especially for `FeatureToggleExtended`

--- src/main/java/com/red/masaadditions/tweakeroo_additions/config/HotkeysExtended.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/config/HotkeysExtended.java
@@ -1,15 +1,19 @@
 package com.red.masaadditions.tweakeroo_additions.config;
 
 import com.google.common.collect.ImmutableList;
+import com.red.masaadditions.MasaAdditions;
+
 import fi.dy.masa.malilib.config.options.ConfigHotkey;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 
 import java.util.List;
 
 public class HotkeysExtended {
-    public static final ConfigHotkey BLINK_DRIVE = new ConfigHotkey("blinkDrive", "", "Teleports the player (using a tp command) to the location the player is looking at.\nPorted from 1.12 Tweakeroo.");
-    public static final ConfigHotkey BLINK_DRIVE_Y_LEVEL = new ConfigHotkey("blinkDriveYLevel", "", "Teleports the player (using a tp command)\nto the location  the player is looking at,\nbut it maintains the current y-position.\nPorted from 1.12 Tweakeroo.");
-    public static final ConfigHotkey REPLACEMENT_MODE = new ConfigHotkey("replacementMode", "", KeybindSettings.MODIFIER_INGAME, "While block placing, replaces the block you're looking at instead of placing against it.\nOnly works if you are opped or in singleplayer.");
+    private static final String HOTKEYS_KEY = MasaAdditions.MOD_ID+ ".config.hotkeys_extended2";
+
+    public static final ConfigHotkey BLINK_DRIVE = new ConfigHotkey("blinkDrive", "").apply(HOTKEYS_KEY);
+    public static final ConfigHotkey BLINK_DRIVE_Y_LEVEL = new ConfigHotkey("blinkDriveYLevel", "").apply(HOTKEYS_KEY);
+    public static final ConfigHotkey REPLACEMENT_MODE = new ConfigHotkey("replacementMode", "", KeybindSettings.MODIFIER_INGAME).apply(HOTKEYS_KEY);
 
     public static final List<ConfigHotkey> EXTENDED_HOTKEY_LIST = ImmutableList.of(
             BLINK_DRIVE,
